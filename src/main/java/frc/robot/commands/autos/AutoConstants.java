c228872d0e0dfad44e3aebdc2bfccc5846c4fdb4
rams5623 <99762986+rams5623@users.noreply.github.com>
2023-03-11 12:50:16 -0500

    work in progress

---
+++ src/main/java/frc/robot/commands/autos/AutoConstants.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.autos;
+
+public final class AutoConstants {
+    /* UNFOLD AUTO COMMAND CONSTANTS */
+    public static class unfoldConst {
+        public static final double kColumnTimeout = 5.0; // [seconds] Timeout for moving the column to unfold position
+        public static final double kColumnStartDelay = 4.5; // [seconds] Delay before moving column in order to prevent boom-column interaction
+        public static final double kBoomTimeout = 14.0; // [seconds] Timeout for moving the boom to unfold position
+    }
+
+    public static class driveConst {
+        public static final double kDriveP = 0.1;
+        public static final double kDriveMaxSpeed = -0.5; // Percentage
+        public static final double kDrivePos = -52; // Inches
+        public static final double kDriveYawPgain = 0.0;
+        public static final double kDriveYawDgain = 0.0;
+    }
+}
---
+++ src/main/java/frc/robot/commands/autos/driveDistance.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.commands.autos.AutoConstants.driveConst;
+import frc.robot.subsystems.Drivetrain;
+
+public class driveDistance extends CommandBase {
+  private final Drivetrain drivetrain;
+  private final Boolean driveStraight;
+  private final Double dist;
+  private Double PIDError;
+  private Double PIDOut;
+
+  public driveDistance(Double p_dist, Boolean p_driveStraight, Drivetrain p_drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    drivetrain = p_drivetrain;
+    driveStraight = p_driveStraight;
+    dist = p_dist;
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    drivetrain.resetEncoder();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    PIDError = drivetrain.getAvgEncoder(); //[Inches]
+    PIDOut = (dist - PIDError) * driveConst.kDriveP;
+    
+    if (PIDOut > driveConst.kDriveMaxSpeed) {
+      PIDOut = driveConst.kDriveMaxSpeed;
+    }
+
+    drivetrain.drive(
+      PIDOut,
+      0.0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivetra
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
